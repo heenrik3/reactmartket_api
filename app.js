7d40af12d793826915ca2ae7971660797436e93c
Henrik3 <henriqlh@gmail.com>
2022-12-08 11:26:29 -0300

    cors installed

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import productRouter from './routes/productRouter.js'
 
 const app = express()
 
+app.use(
+  cors({
+    origin: 'https://reactmarket-seven.vercel.app/',
+  })
+)
 app.use('/', (req, res, next) => {
   console.log(`Request ${req.method} at ${req.url}`)
 
